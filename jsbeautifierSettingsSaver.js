897847a9486c12be3470a9dbff9b70d9ca94fa9c
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-18 11:55:15 -0700

    initial commit

---
+++ jsbeautifierSettingsSaver.js
@@ -0,0 +1,13 @@
+(function () {
+
+  'use strict';
+
+  // set any vars you want to change here
+  var jslintCheckbox = document.getElementById('jslint-happy');
+  var tabSize = document.getElementById('tabsize');
+
+  // set your values for those vars here
+  tabSize.value = 2;
+  jslintCheckbox.checked = true;
+
+}());
