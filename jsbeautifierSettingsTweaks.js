e508039c78891736b9572b5e14bb6bb0d85561e8
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-18 14:13:51 -0700

    wrap in DOMContentLoaded listener

--- jsbeautifierSettingsTweaks.js
+++ jsbeautifierSettingsTweaks.js
@@ -1,15 +1,16 @@
 /*! jsbeautifierSettingsTweaks.js v0.1.0 by ryanpcmcquen */
-
 (function () {
 
   'use strict';
 
-  // set any vars you want to change here
-  var jslintCheckbox = document.getElementById('jslint-happy');
-  var tabSize = document.getElementById('tabsize');
+  document.addEventListener('DOMContentLoaded', function () {
+    // set any vars you want to change here
+    var jslintCheckbox = document.getElementById('jslint-happy');
+    var tabSize = document.getElementById('tabsize');
 
-  // set your values for those vars here
-  tabSize.value = 2;
-  jslintCheckbox.checked = true;
+    // set your values for those vars here
+    tabSize.value = 2;
+    jslintCheckbox.checked = true;
+  });
 
 }());
