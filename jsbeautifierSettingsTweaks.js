4bc6d8e1bca6e4f35a6faa630135849a197bda11
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-06-29 15:04:19 -0700

    Make it 4 spaces.

--- jsbeautifierSettingsTweaks.js
+++ jsbeautifierSettingsTweaks.js
@@ -1,13 +1,13 @@
 /*! jsbeautifierSettingsTweaks.js v0.3.0 by ryanpcmcquen */
 window.addEventListener('load', function () {
-  'use strict';
-  // Set any vars you want to change here:
-  var jslintCheckbox = document.getElementById('jslint-happy');
-  var tabSize = document.getElementById('tabsize');
-  var wrapLength = document.getElementById('wrap-line-length');
+    'use strict';
+    // Set any vars you want to change here:
+    var jslintCheckbox = document.getElementById('jslint-happy');
+    var tabSize = document.getElementById('tabsize');
+    var wrapLength = document.getElementById('wrap-line-length');
 
-  // Set your values for those vars here:
-  tabSize.value = 4;
-  jslintCheckbox.checked = true;
-  wrapLength.value = 120;
+    // Set your values for those vars here:
+    tabSize.value = 4;
+    jslintCheckbox.checked = true;
+    wrapLength.value = 120;
 });
